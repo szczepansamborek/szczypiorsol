b58b808891b2358b96267d6180ccd96f2957b6a4
szczepansamborek <60288963+szczepansamborek@users.noreply.github.com>
2020-01-25 19:24:16 +0100

    Add files via upload

---
+++ _2_OddOccurencyInArray.py
@@ -0,0 +1,11 @@
+def solution(A):
+    # write your code in Python 3.6
+    A.sort()
+    for i in range(0, len(A), 2):
+        if A[i] != A[i+1]:
+           return A[i]
+    return -1
+
+print(1)
+print(7)
+print(solution([9, 3, 9, 3, 7, 9]))
