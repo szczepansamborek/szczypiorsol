fb89a981fa2e7556b4f74b38c57df68ff9548ef1
szczepansamborek <60288963+szczepansamborek@users.noreply.github.com>
2020-02-18 20:08:48 +0100

    Update _6_MaxProductOfThree.py

--- _6_MaxProductOfThree.py
+++ _6_MaxProductOfThree.py
@@ -1,8 +1,10 @@
 def solution(A):
     A.sort()
-    if(A[0] * A[1] > A[-2] * A[-3]):
+    if(A[0] * A[1] > A[-2] * A[-3] and A[-1] > 0):
         return A[0] * A[1] * A[-1]
     else:
         return A[-3] * A[-2] * A[-1]
 
 print(solution([-3, 1, 2, -2, 5, 6]))
+print(solution([-3, -1, -2, -2, -5, -6]))
+print(solution([-3, -1, -2, -2, -5, 6]))
